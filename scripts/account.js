b8d9b54d1eb7f8df75ff30a39307dbc63fd3b576
kainev11 <eannikolaev@gmail.com>
2021-07-01 10:02:56 +0500

    add scripts foe profile and notifications

--- scripts/account.js
+++ scripts/account.js
@@ -1,8 +1,23 @@
 let profileBlock = document.querySelector(".header__nav_profile");
 let button = profileBlock.querySelector(".header__nav_profile-button");
-let listOpened = false;
-button.addEventListener("click", function() {
-    listOpened = !listOpened;
+let profileMenu = profileBlock.querySelector(".header__nav_profile-menu");
+button.addEventListener("click", function () {
     button.classList.toggle("header__nav_profile-button-opened");
+    profileMenu.classList.toggle("header__nav_profile-menu-opened");
+});
+
+let notificationButton = document.querySelector(".header__nav_menu-button");
+let notificationCount = document.querySelectorAll(".header__nav_menu-notification.new").length;
+let notifications = document.querySelectorAll(".header__nav_menu-notification");
+let notificationBlock = document.querySelector(".header__nav_menu-notifications");
+let notificationCounter = notificationButton.querySelector(".header__nav_menu-notifications-counter");
+notificationButton.addEventListener("click", function () {
+    notificationButton.classList.toggle("header__nav_menu-button-active");
+    notificationBlock.classList.toggle("header__nav_menu-notifications-opened");
+    notificationCounter.innerHTML = 0;
+    if (!notificationButton.classList.contains("header__nav_menu-button-active")) {
+        for (let i = 0; i < notificationCount; i++) 
+            notifications[i].classList.remove("new");
+    }
     
 });
