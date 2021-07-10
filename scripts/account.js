cee269cea39ff3d6c0fba0f910df132bc278b0a9
kainev11 <eannikolaev@gmail.com>
2021-07-10 18:38:23 +0500

    add correct drop-down list for statistics

--- scripts/account.js
+++ scripts/account.js
@@ -15,8 +15,42 @@ notificationButton.addEventListener("click", function () {
     notificationButton.classList.toggle("header__nav_menu-button-active");
     notificationBlock.classList.toggle("header__nav_menu-notifications-opened");
     if (!notificationButton.classList.contains("header__nav_menu-button-active")) {
-        for (let i = 0; i < notificationCount; i++) 
+        for (let i = 0; i < notificationCount; i++)
             notifications[i].classList.remove("new");
     }
     notificationCounter.style.display = "none";
 });
+
+let teams = document.querySelectorAll(".content__results_team");
+let teamLists = document.querySelectorAll(".content__results_team-list");
+let currentTeam = -1;
+let closed = true;
+for (let i = 0; i < teams.length; i++) {
+    teams[i].addEventListener("click", function () {
+        if (!closed) {
+            teams[currentTeam].classList.remove("content__results_team-opened");
+            teamLists[currentTeam].classList.remove("opened");
+            teamLists[currentTeam].querySelector(".content__results_team-teammates").classList.remove("opened");
+            let teammatesList = teamLists[currentTeam].querySelectorAll(".content__results_team-teammate");
+            for (let teammate of teammatesList) {
+                teammate.classList.remove("opened");
+            }
+            teamLists[currentTeam].querySelector("img").classList.remove("opened");
+            closed = !closed;
+            if (currentTeam === i)
+                return;
+        }
+        if (closed) {
+            teams[i].classList.add("content__results_team-opened");
+            teamLists[i].classList.add("opened");
+            teamLists[i].querySelector(".content__results_team-teammates").classList.add("opened");
+            teammatesList = teamLists[i].querySelectorAll(".content__results_team-teammate");
+            for (let teammate of teammatesList) {
+                teammate.classList.add("opened");
+            }
+            teamLists[i].querySelector("img").classList.add("opened");
+            closed = !closed;
+        }
+        currentTeam = i;
+    });
+}
